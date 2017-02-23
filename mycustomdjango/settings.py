092f80d12f7ad01834f98f43cea3650d0d12a41f
Francesco Bartoli <francesco.bartoli@geobeyond.it>
2017-02-23 09:33:27 +0100

    Set static url in GAE

--- mycustomdjango/settings.py
+++ mycustomdjango/settings.py
@@ -20,7 +20,8 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
+STATIC_URL = 'http://storage.googleapis.com/mycustomdjango/static/'
 
 # STATICFILES_DIRS = (
 #     os.path.join(PROJECT_ROOT, 'static'),
