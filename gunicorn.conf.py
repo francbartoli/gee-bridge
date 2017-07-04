83b84be243ae01311f6adba290b954f50bff0157
Bartoli, Francesco (CIO) <Francesco.Bartoli@fao.org>
2017-07-04 11:40:31 +0200

    Add options to gunicorn configuration for debugging

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,4 +1,8 @@
 bind = '0.0.0.0:9000'
 # bind = 'unix:/tmp/supervisor.sock'
 workers = 3
-timeout = 60
+timeout = 120
+capture_output = True
+loglevel = "debug"
+errorlog = '/tmp/gunicorn_error.log'
+daemon = True
