6a7395e2f2b8828bdb622b5de89ab0eb4a83bc22
Francesco Bartoli <francesco.bartoli@geobeyond.it>
2017-11-08 16:08:49 +0100

    Fix gunicorn capture output for internal use

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,8 +1,8 @@
 bind = '0.0.0.0:9000'
-# bind = 'unix:/tmp/supervisor.sock'
-workers = 3
+#bind = 'unix:/tmp/supervisor.sock'
+workers = 5
 timeout = 120
-capture_output = True
-loglevel = "debug"
-errorlog = '/tmp/gunicorn_error.log'
-daemon = True
+#capture_output = True
+#loglevel = "debug"
+#errorlog = '/tmp/gunicorn_error.log'
+#daemon = False
