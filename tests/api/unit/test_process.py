80c885ff3b0b8cb6826060e60a113fd128adfdff
Francesco Bartoli <francesco.bartoli@geobeyond.it>
2018-10-24 22:55:30 +0200

    Add basic test for process model

---
+++ tests/api/unit/test_process.py
@@ -0,0 +1,11 @@
+import pytest
+from tests.api.factories import ProcessFactory
+
+
+@pytest.mark.django_db
+def test_process_model():
+    """ Test process model """
+    # create process model instance
+    process = ProcessFactory()
+    assert process.type["mode"] == "sync"
+    assert 'wapor' in process.type
