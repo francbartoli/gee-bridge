00637ba55f4d88f570268daba65f8beb28ab48c1
Francesco Bartoli <francesco.bartoli@geobeyond.it>
2018-10-24 11:05:11 +0200

    Add process model to django admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from api.models import Process
 # Register your models here.
+
+
+class ProcessAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'name', 'type', 'owner', 'aoi',
+        'toi', 'input_data', 'output_data'
+    )
+
+
+admin.site.register(Process, ProcessAdmin)
