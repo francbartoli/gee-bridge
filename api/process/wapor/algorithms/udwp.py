cff8db48146e17cd994e15b3d7886fd622576283
Francesco Bartoli <francesco.bartoli@geobeyond.it>
2018-10-31 19:25:13 +0100

    Add logger and temporal filter

--- api/process/wapor/algorithms/udwp.py
+++ api/process/wapor/algorithms/udwp.py
@@ -7,23 +7,31 @@ from rest_framework.serializers import ValidationError
 class UDWP:
 
     def __init__(self, **kwargs):
-        print(kwargs)
-        # self.logger = logging.getLogger(__name__, subsystem="algorithms")
+
+        self.logger = logging.getLogger(__name__)
         self.__name = "UDWP"
         # filters
         self.__filters = {}
+        self.logger.debug("Received kwargs are:\n{}".format(kwargs))
         try:
             if kwargs["filters"]:
                 for filter_k, filter_v in kwargs["filters"].items():
-                    # extract spatial filter if polygon type
+                    # extract spatial filter if polygon type and add it
                     if filter_k == "spatial_extent":
                         if self.is_polygon(filter_v):
                             self.__filters['{}'.format(filter_k)] = filter_v
                         else:
+                            self.logger.error(
+                                "GeoJSON type from aoi is not of type Polygon"
+                            )
                             raise ValidationError(
                                 "Area of interest is not a Polygon"
                             )
+                    # extract temporal filter and add it
+                    if filter_k == "temporal_extent":
+                        self.__filters['{}'.format(filter_k)] = filter_v
         except KeyError as e:
+            self.logger.exception("KeyError exception")
             raise
         self.__outputs = {"maps": {}, "stats": {}, "tasks": {}}
         # FIXME into marmee so we can initialize self.__errors as {}
@@ -37,6 +45,7 @@ class UDWP:
         """Retrieve if the spatial extent is of polygon type"""
 
         type = GeoJsonUtil(obj).type
+        self.logger.debug("GeoJSON type is {}".format(type))
         if type is not Polygon:
             return False
         else:
