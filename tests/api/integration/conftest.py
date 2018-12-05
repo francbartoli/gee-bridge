307666a3d19fcd97b35c7590b276c5ecf8ecd61b
Francesco Bartoli <francesco.bartoli@geobeyond.it>
2018-12-05 15:14:54 +0100

    Make test passing for process not owned

--- tests/api/integration/conftest.py
+++ tests/api/integration/conftest.py
@@ -1,16 +1,21 @@
 import pytest
 from pytest_factoryboy import register
-from tests.api.factories import ProcessFactory, UserFactory
+from tests.api.factories import ProcessFactory, UserFactory, TokenFactory
+from django.contrib.auth.models import User
 from rest_framework.test import APIClient
+from rest_framework.reverse import reverse
 
 
 register(UserFactory, "test_user")
+register(TokenFactory)
 register(ProcessFactory)
 
 
 @pytest.fixture
 def user(test_user):
     test_user.username = "test"
+    # save method has to be called otherwise the value is not persisted
+    test_user.save()
     return test_user
 
 
@@ -26,5 +31,28 @@ def user_api_client(user, api_client):
 
 
 @pytest.fixture
+def jwt_api_client(user, api_client):
+    url = reverse("rest_login")
+    data = {
+        "username": user.username,
+        "email": user.email,
+        "password": "password"
+    }
+    resp = api_client.post(url, data=data, format="json")
+    jwt = resp.json()["token"]
+    # same as headers = {'Authorization': 'JWT {}'.format(jwt)}
+    api_client.credentials(HTTP_AUTHORIZATION='JWT {}'.format(jwt))
+    return api_client
+
+
+@pytest.fixture
 def create_process(user):
+    return ProcessFactory(name="test_process", owner=user)
+
+
+@pytest.fixture
+def create_notowned_process():
+    username = "kotest"
+    UserFactory(username=username)
+    user = User.objects.filter(username=username).first()
     return ProcessFactory(name="test_process", owner=user)
--- tests/api/integration/test_views.py
+++ tests/api/integration/test_views.py
@@ -1,14 +1,23 @@
 import pytest
-from django.urls import reverse
+from rest_framework.reverse import reverse
 from rest_framework import status
 
 
 class TestProcessList:
 
     @pytest.mark.django_db
-    def test_can_get_process_list(self, user_api_client, create_process):
+    def test_user_can_get_process_list(self, user_api_client, create_process):
         url = reverse('process-list')
         response = user_api_client.get(url)
         assert response.status_code == status.HTTP_200_OK
         assert len(response.json()) == 1
         assert response.json()[0]['name'] == create_process.name
+
+    @pytest.mark.django_db
+    def test_user_cannot_get_notowned_process_list(
+        self, jwt_api_client, create_notowned_process
+    ):
+        url = reverse('process-list')
+        response = jwt_api_client.get(url)
+        assert response.status_code == status.HTTP_200_OK
+        assert len(response.json()) == 0
